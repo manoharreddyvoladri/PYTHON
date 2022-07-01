df7141abaa4b90d695d6e354a58980032b8c352e
manoharreddyvoladri <92422839+manoharreddyvoladri@users.noreply.github.com>
2022-07-01 21:17:17 +0530

    Update dice number genrator.py

--- dice number genrator.py
+++ dice number genrator.py
@@ -1,9 +1,10 @@
 import random 
 while True:     
-     print(''' 1. roll the dice             2. exit     ''')    
-     user = int(input("what you want to do\n"))     
-     if user==1:         
+     print('''1. Roll the dice \n''')
+     print('''2. Exit''')    
+     num = int(input("what you want to do\n"))     
+     if num==1:         
         number = random.randint(1,6)         
         print(number)     
-     else:         
+     else:
         break
